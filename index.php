057f9d07a088dec6759abf377b9ce352bae8a23f
Mafer2409 <104541275+Mafer2409@users.noreply.github.com>
2023-06-09 14:33:27 +0800

    new

--- index.php
+++ index.php
@@ -1,85 +1,288 @@
-<?php
-session_start();
-session_destroy();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-    <meta name="author" content="AdminKit">
-    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+    <title>Puskesmas Lewolaga</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="shortcut icon" href="admin/img/logo/puskesmas-logo.png" />
+    <!-- Favicon -->
+    <link href="admin/img/logo/puskesmas-logo.png" rel="icon">
 
-    <link rel="canonical" href="https://demo-basic.adminkit.io/pages-sign-in.html" />
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
 
-    <title>Sign In | Puskesmas Lewolaga</title>
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-    <link href="admin/css/app.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    <!-- Libraries Stylesheet -->
+    <link href="assets-template/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="assets-template/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets-template/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="assets-template/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="assets-template/css/style.css" rel="stylesheet">
 </head>
 
 <body>
-    <main class="d-flex w-100">
-        <div class="container d-flex flex-column">
-            <div class="row vh-100">
-                <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
-                    <div class="d-table-cell align-middle">
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
+    </div>
+    <!-- Spinner End -->
 
-                        <div class="text-center mt-4">
-                            <h1 class="h2">Haii! Welcome back...</h1>
-                            <p class="lead">
-                                Sign in to your account to continue
-                            </p>
-                        </div>
 
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="m-sm-4">
-                                    <div class="text-center">
-                                        <img src="admin/img/logo/puskesmas-logo.png" alt="Charles Hall" class="img-fluid rounded" width="132" height="132" />
-                                    </div>
-                                    <form action="login.php" method="post">
-                                        <div class="mb-3">
-                                            <label class="form-label">Akses</label>
-                                            <select class="form-control form-control-lg" name="akses" required>
-                                                <option value="">-- Pilih Akses --</option>
-                                                <option value="admin">Admin</option>
-                                                <option value="paramedis">Paramedis</option>
-                                                <option value="kepala">Kepala Puskesmas</option>
-                                            </select>
-                                        </div>
-                                        <div class="mb-3">
-                                            <label class="form-label">Email</label>
-                                            <input class="form-control form-control-lg" type="email" name="email" placeholder="Enter your email" required />
-                                        </div>
-                                        <div class="mb-3">
-                                            <label class="form-label">Password</label>
-                                            <input class="form-control form-control-lg" type="password" name="password" placeholder="Enter your password" required />
-                                        </div>
-                                        <div class="text-center mt-3">
-                                            <input type="submit" class="btn btn-primary btn-block" name="login" value="Sign In!">
-                                            <!-- <button type="submit" class="btn btn-lg btn-primary">Sign in</button> -->
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-primary text-white d-none d-lg-flex">
+        <div class="container py-3">
+            <div class="d-flex align-items-center">
+                <a href="index.php">
+                    <h2 class="text-white fw-bold m-0">Puskesmas Lewolaga</h2>
+                </a>
+                <div class="ms-auto d-flex align-items-center">
+                    <small class="ms-4"><i class="fa fa-map-marker-alt me-3"></i>Jl. Negara Trans, Lewolaga, Flores Timur</small>
+                    <!-- <small class="ms-4"><i class="fa fa-envelope me-3"></i>info@example.com</small> -->
+                    <small class="ms-4"><i class="fa fa-phone-alt me-3"></i>+62 812 3710 7839</small>
+                    <div class="ms-3 d-flex">
+                        <a href="index2.php" class="btn btn-light rounded-pill py-2 px-3 mt-2 mb-2">Sign In!</a>
+                        <!-- <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-sm-square btn-light text-primary rounded-circle ms-2" href=""><i class="fab fa-linkedin-in"></i></a> -->
                     </div>
                 </div>
             </div>
         </div>
-    </main>
+    </div>
+    <!-- Topbar End -->
 
-    <script src="js/app.js"></script>
 
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-white sticky-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light p-lg-0">
+                <a href="index.html" class="navbar-brand d-lg-none">
+                    <h1 class="fw-bold m-0">Puskesmas Lewolaga</h1>
+                </a>
+                <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav">
+                        <!-- <a href="index.html" class="nav-item nav-link active">Home</a>
+                        <a href="about.html" class="nav-item nav-link">About</a>
+                        <a href="service.html" class="nav-item nav-link">Services</a>
+                        <a href="project.html" class="nav-item nav-link">Projects</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu bg-light rounded-0 rounded-bottom m-0">
+                                <a href="feature.html" class="dropdown-item">Features</a>
+                                <a href="team.html" class="dropdown-item">Our Team</a>
+                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                <a href="quote.html" class="dropdown-item">Quotation</a>
+                                <a href="404.html" class="dropdown-item">404 Page</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" class="nav-item nav-link">Contact</a> -->
+                    </div>
+                    <div class="ms-auto d-none d-lg-block">
+                        <!-- <a href="" class="btn btn-primary rounded-pill py-2 px-3 mt-2 mb-2">Get A Quote</a> -->
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start -->
+    <div class="container-fluid px-0 mb-5">
+        <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="assets-template/img/carousel-1.jpg" alt="Image">
+                    <div class="carousel-caption">
+                        <div class="container">
+                            <div class="row justify-content-start">
+                                <div class="col-lg-7 text-start">
+                                    <p class="fs-4 text-white animated slideInRight">Selamat datang di...</p>
+                                    <h1 class="display-1 text-white mb-4 animated slideInRight">Puskesmas Lewolaga</h1>
+                                    <a href="index2.php" class="btn btn-primary rounded-pill py-3 px-5 animated slideInRight">Sign In!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="w-100" src="assets-template/img/carousel-2.jpg" alt="Image">
+                    <div class="carousel-caption">
+                        <div class="container">
+                            <div class="row justify-content-end">
+                                <div class="col-lg-7 text-end">
+                                    <p class="fs-4 text-white animated slideInLeft">Selamat datang di...</p>
+                                    <h1 class="display-1 text-white mb-5 animated slideInLeft">Puskesmas Lewolaga</h1>
+                                    <a href="index2.php" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Sign In!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+
+    <!-- Features Start -->
+    <div class="container-xxl py-5">
+        <div class="container mb-3" align="center">
+            <h3>Pelayanan kami :</h3>
+        </div>
+        <div class="container">
+            <div class="row g-0 feature-row">
+                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
+                    <div class="feature-item border h-100 p-5">
+                        <div class="btn-square bg-light rounded-circle mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="assets-template/img/icon/icon-2.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Poli Umum</h5>
+                        <!-- <p class="mb-0">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam</p> -->
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
+                    <div class="feature-item border h-100 p-5">
+                        <div class="btn-square bg-light rounded-circle mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="assets-template/img/icon/icon-2.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Poli Gigi</h5>
+                        <!-- <p class="mb-0">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam</p> -->
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
+                    <div class="feature-item border h-100 p-5">
+                        <div class="btn-square bg-light rounded-circle mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="assets-template/img/icon/icon-2.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Poli KIA/KB</h5>
+                        <!-- <p class="mb-0">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam</p> -->
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
+                    <div class="feature-item border h-100 p-5">
+                        <div class="btn-square bg-light rounded-circle mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="assets-template/img/icon/icon-2.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Poli Gizi</h5>
+                        <!-- <p class="mb-0">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam</p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Features End -->
+
+    <!-- Quote Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <h1 class="display-5 mb-4">Visi :</h1>
+                    <h3>"Terwujudnya Masyarakat Titehena Yang Sehat, Mandiri, Keadilan dan Missioner."</h3>
+                    <!-- <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo</p> -->
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <h1 class="display-5 mb-4">Misi :</h1>
+                    <p class="text-dark">1. Pemberdayaan masyarakat melalui program-program pelayanan kesehatan.</p>
+                    <p class="mb-4 text-dark">2. Peningkatan pelayanan kesehatan yang prima, professional (sikap dan keterampilan).</p>
+                    <p class="mb-4 text-dark">3. Menjalin kerjasama lintas sektor secara bertangung jawab.</p>
+                    <p class="mb-4 text-dark">4. Membangun kemitraan dengan multi pihak (tokoh agama, tokoh adat, tokoh masyarakat dan lembaga swadaya masyarakat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Quote Start -->
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-white mb-4">Puskesmas Lewolaga</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Jl. Negara Trans, Lewolaga, Flores Timur</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+62 812 3710 7839</p>
+                    <!-- <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p> -->
+                    <!-- <div class="d-flex pt-3">
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div> -->
+                </div>
+                <div class="col-lg-4 col-md-6">
+
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <!-- <h4 class="text-white mb-4">Business Hours</h4> -->
+                    <p class="mb-1">Senin - Minggu</p>
+                    <h6 class="text-light">24 Jam</h6>
+                    <!-- <p class="mb-1">Saturday</p>
+                    <h6 class="text-light">09:00 am - 12:00 pm</h6>
+                    <p class="mb-1">Sunday</p>
+                    <h6 class="text-light">Closed</h6> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Copyright Start -->
+    <div class="container-fluid copyright py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a class="fw-medium text-light" href="#">Puskesmas Lewolaga</a>, All Right Reserved.
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <!-- Designed By <a class="fw-medium text-light" href="https://htmlcodex.com">HTML Codex</a>
+                    Distributed By <a class="fw-medium text-light" href="https://themewagon.com">ThemeWagon</a> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets-template/lib/wow/wow.min.js"></script>
+    <script src="assets-template/lib/easing/easing.min.js"></script>
+    <script src="assets-template/lib/waypoints/waypoints.min.js"></script>
+    <script src="assets-template/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="assets-template/lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="assets-template/js/main.js"></script>
 </body>
 
 </html>
