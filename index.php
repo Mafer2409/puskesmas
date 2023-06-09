d16e292cc533ab4e8725a46f906eff2596c50e78
Mafer2409 <104541275+Mafer2409@users.noreply.github.com>
2023-06-09 14:56:02 +0800

    vv

--- index.php
+++ index.php
@@ -107,13 +107,13 @@
         <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="assets-template/img/carousel-1.jpg" alt="Image">
+                    <img class="w-100" src="assets/img/gbr-puskesmas.JPG" alt="Image">
                     <div class="carousel-caption">
                         <div class="container">
                             <div class="row justify-content-start">
                                 <div class="col-lg-7 text-start">
-                                    <p class="fs-4 text-white animated slideInRight">Selamat datang di...</p>
-                                    <h1 class="display-1 text-white mb-4 animated slideInRight">Puskesmas Lewolaga</h1>
+                                    <p class="fs-4 text-white animated slideInRight" style="text-shadow: 2px 2px #696969;" style="text-shadow: 2px 2px #696969;">Selamat datang di...</p>
+                                    <h1 class="display-1 text-white mb-4 animated slideInRight" style="text-shadow: 4px 4px #696969;">Puskesmas Lewolaga</h1>
                                     <a href="index2.php" class="btn btn-primary rounded-pill py-3 px-5 animated slideInRight">Sign In!</a>
                                 </div>
                             </div>
@@ -121,13 +121,13 @@
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img class="w-100" src="assets-template/img/carousel-2.jpg" alt="Image">
+                    <img class="w-100" src="assets/img/gbr-puskesmas.JPG" alt="Image">
                     <div class="carousel-caption">
                         <div class="container">
                             <div class="row justify-content-end">
                                 <div class="col-lg-7 text-end">
-                                    <p class="fs-4 text-white animated slideInLeft">Selamat datang di...</p>
-                                    <h1 class="display-1 text-white mb-5 animated slideInLeft">Puskesmas Lewolaga</h1>
+                                    <p class="fs-4 text-white animated slideInLeft" style="text-shadow: 2px 2px #696969;">Selamat datang di...</p>
+                                    <h1 class="display-1 text-white mb-5 animated slideInLeft" style="text-shadow: 4px 4px #696969;">Puskesmas Lewolaga</h1>
                                     <a href="index2.php" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Sign In!</a>
                                 </div>
                             </div>
